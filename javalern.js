3649727137d7b05ec3958f29c5e21707584e0cc1
yanpogurdanidani <yan.dani.programm@gmail.com>
2024-07-14 19:00:51 +0200

    Add files via upload

---
+++ javalern.js
@@ -0,0 +1,21 @@
+
+
+function like() {
+	var car = true;
+	while(car){
+	alert(": D");
+	}
+}
+function rife() {
+	var data = confirm("Ты точно ето хочешь?");
+	if(data){
+	var data2 = confirm("Уверен?");
+		if(data2) {
+			like()
+		}else{
+	alert("Ну ладно :(");
+}
+}else{
+	alert("Ну ладно :(");
+}
+}
